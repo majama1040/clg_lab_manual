44555ef38ddcaaeefa00022b269044f8bbf50f71
majama1040 <120716021+majama1040@users.noreply.github.com>
2025-09-02 15:27:43 +0530

    Add files via upload

---
+++ 224ce8/matrix_chain.java
@@ -0,0 +1,63 @@
+
+public class matrix_chain {
+
+    public static void main(String[] args) {
+        String a1 = "5*4";
+        String a2 = "4*6";
+        String a3 = "6*2";
+        String a4 = "2*7";
+
+        int p1 = a1.charAt(0) - '0';
+        int p2 = a2.charAt(0) - '0';
+        int p3 = a3.charAt(0) - '0';
+        int p4 = a4.charAt(0) - '0';
+        int p5 = a4.charAt(2) - '0';
+
+        int[] p = {p1, p2, p3, p4, p5};
+
+        System.out.println(p5);
+
+        int[][] m = new int[5][5];
+        int[][] s = new int[5][5];
+
+        for (int q = 2; q < s.length; q++) {
+            for (int i = 1; i < s.length-q+1; i++) {
+                int j =i+q-1;
+
+                if (i == j) {
+                    m[i][j] = 0;
+                    //System.out.println(i+" "+j);
+                } else {
+
+                    int k = i;
+                    int cost = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
+                    int org_cost = cost;
+                    for (int l = k+1; l <= j-1; l++) {
+                        cost = Math.min(cost, m[i][l] + m[l + 1][j] + p[i - 1] * p[l] * p[j]);
+                        System.out.println(i + ":i " + j + ":j " + ":k " + k + " " + cost);
+                        if (org_cost != cost) {
+                            k=l;
+                        }
+                    }
+                    s[i][j] = k;
+                    m[i][j] = cost;
+                }
+            }
+        }
+
+        for (int i = 0; i < s.length; i++) {
+            for (int j = 0; j < s.length; j++) {
+                System.out.print(m[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("\n\n\n");
+
+        for (int i = 0; i < s.length; i++) {
+            for (int j = 0; j < s.length; j++) {
+                System.out.print(s[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
